b7991cb17a049646d37aadf7d00ff41cc04aca7b
TypeAris <34433184+TypeAris@users.noreply.github.com>
2017-12-11 16:44:12 +0900

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+package gitTest;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!!!!!!");
+		System.out.println("안녕하세요");
+		System.out.println("마스터 작업중");
+	}
+	public int sum() {
+		return 3+5;
+	}
+
+	public int multi() {
+		return 5*3;
+	}
+	public int sub() {
+		return 5-3;
+	}
+}
